7f8655d25860c519823e18c60c29115bbbaec0aa
dnzhelloo <dnzhelloo@icloud.com>
2023-03-23 13:50:50 +0300

    create model to use git api

---
+++ gitFollowersApp/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  gitFollowersApp
+//
+//  Created by deniz on 23.03.2023.
+//
+
+import Foundation
+
+struct Follower:Codable{
+    var login:String?
+    var avatarUrl:String?
+    
+}
